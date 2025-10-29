86f4b088eb7a74aa502a0bacb44116c6a88bd65e
Sylvain <dev@sylvainconny.com>
2025-10-29 14:15:44 +0100

    push / pull from drive

---
+++ pull.sh
@@ -0,0 +1,3 @@
+cp -r /Volumes/Conny-SD/01_eanis/02_cours/content/* ./content
+cp -r /Volumes/Conny-SD/01_eanis/02_cours/static/* ./static
+rm -rf */**/._*
---
+++ push.sh
@@ -0,0 +1,4 @@
+rm -rf */**/._*
+rm -rf /Volumes/Conny-SD/01_eanis/02_cours/*
+cp -r ./content /Volumes/Conny-SD/01_eanis/02_cours/
+cp -r ./static /Volumes/Conny-SD/01_eanis/02_cours/
