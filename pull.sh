0be04f8ab0be9a3fdcd6d1ed992b94afaf645c09
Sylvain <dev@sylvainconny.com>
2025-11-15 10:17:11 +0100

    update scripts

--- pull.sh
+++ pull.sh
@@ -1,5 +1,4 @@
 git pull
-cp -r /run/media/sylvain/Conny-SD/01_eanis/02_cours/content/* ./content
-cp -r /run/media/sylvain/Conny-SD/01_eanis/02_cours/static/* ./static
+cp -r /run/media/sylvain/Conny-SD/01_eanis/02_cours/* ./content
 rm -rf */**/._*
 rm -r ./public*
--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
 rm -rf */**/._*
 rm -rf /run/media/sylvain/Conny-SD/01_eanis/02_cours/*
-cp -r ./content /run/media/sylvain/Conny-SD/01_eanis/02_cours/
-cp -r ./static /run/media/sylvain/Conny-SD/01_eanis/02_cours/
+cp -r ./content/* /run/media/sylvain/Conny-SD/01_eanis/02_cours/
